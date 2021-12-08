6aeddad85c0042645de603e6b85780379e0f7230
GitHub Action <action@github.com>
2021-12-08 08:11:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/jwoyo/werkbank/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jwoyo/werkbank/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jwoyo.werkbank.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jwoyo.werkbank.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/jwoyo/werkbank/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jwoyo/werkbank/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jwoyo.werkbank.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jwoyo.werkbank.MyBundle
 
 class MyApplicationService {
 
